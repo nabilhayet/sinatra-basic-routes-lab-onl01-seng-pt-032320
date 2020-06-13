c13c4851cabc4d326c7a62f3f6f16124ec530ef5
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-13 01:17:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get 'name' do 
+    "My name is Nabil"
+  end 
+
+  get 'hometown' do 
+    "My hometown is Noakhali"
+  end 
+
+  get 'favorite-song' do 
+    "My favorite song is kal ho na ho"
+  end 
 end
